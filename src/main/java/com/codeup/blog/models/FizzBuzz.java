aeeda32042b7d594e07ec5681c0ec3c8827c59dd
benny-fields3rd <benny.fields3rd@gmail.com>
2019-01-15 21:25:06 -0600

    working security lesson

---
+++ src/main/java/com/codeup/blog/models/FizzBuzz.java
@@ -0,0 +1,35 @@
+package com.codeup.blog.models;
+
+public class FizzBuzz {
+
+    public static void main(String[] args){
+        // FizzBuzz
+//        for(int i = 1; i <= 100; i ++){
+//            if(i % 15 ==0) {
+//                System.out.println("Fizz Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+        // Count down from 100
+//            System.out.println("Output is: ");
+//
+//            for(int i = 100; i >= 1; i --){
+//                System.out.println(i);
+//            }
+
+        // Count up to 100
+//        System.out.println("Output is: ");
+//
+//        for(int i = 1; i <= 100; i ++){
+//            System.out.println(i);
+//        }
+
+
+        
+    }
+}
